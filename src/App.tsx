a82a06b0bab7183b95a6b6690fd901d46f8b3e6a
VailX <winsenxie95@gmail.com>
2025-05-27 17:19:00 +0800

    change text

--- src/App.tsx
+++ src/App.tsx
@@ -93,8 +93,8 @@ function App() {
         <header>
           <img src="./hero.png" alt="Hero Banner" />
           <h1>
-            Find <span className="text-gradient">Moviesss</span> You'll Enjoy
-            Without Hassle
+            Discover New <span className="text-gradient">Movies</span> You'll
+            Enjoy Without Hassle
           </h1>
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
